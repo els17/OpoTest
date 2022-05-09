403ba62a4b3a5ab2dbcd8e7935ab64789fe91060
els17 <elena_liza15@hotmail.com>
2022-05-09 15:47:38 +0200

    Cargas automatica de los test funcionando

--- OpoTest/src/main/java/com/example/opotest/TestPreliminar.java
+++ OpoTest/src/main/java/com/example/opotest/TestPreliminar.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.opotest.db.dbPreguntas;
 
@@ -32,13 +34,31 @@ public class TestPreliminar extends AppCompatActivity {
 
     public void cargarTests()
     {
-        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.layout);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
         int numTests;
         final dbPreguntas dbPreguntas = new dbPreguntas(TestPreliminar.this);
         numTests = dbPreguntas.cuentaPreguntas(2)/10;
 
+        for (int i = 0; i < numTests; i++)
+        {
+            ImageView img = new ImageView(this);
+            img.setBackgroundResource(R.mipmap.test2);
+            img.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+            layout.addView(img);
+            TextView txt = new TextView(this);
+            txt.setText("Test " + (i+1));
+            txt.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+            layout.addView(txt);
+        }
+        /*
         ImageView img = new ImageView(this);
         img.setBackgroundResource(R.mipmap.test2);
         img.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+        TextView txt = new TextView(this);
+        txt.setText("pruebitasss");
+        txt.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+        layout.addView(txt);
+        layout.addView(img);
+        */
     }
 }
